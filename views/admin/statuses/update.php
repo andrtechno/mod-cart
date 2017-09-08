9faf9dad3d4b53a467ef02f343cbe83e922000eb
andrtechno <andrew.panix@gmail.com>
2017-09-08 21:39:09 +0300

    init

--- views/admin/statuses/update.php
+++ views/admin/statuses/update.php
@@ -3,7 +3,6 @@
 use yii\helpers\Html;
 use panix\engine\bootstrap\ActiveForm;
 use panix\ext\colorpicker\Colorpicker;
-
 ?>
 
 <div class="panel panel-default">
@@ -21,10 +20,10 @@ use panix\ext\colorpicker\Colorpicker;
         <?= $form->field($model, 'name')->textInput(['maxlength' => 100]) ?>
         <?php //echo $form->field($model, 'color')->textInput(['maxlength' => 7]) ?>
         <?php
-           echo $form->field($model, 'color')->widget(Colorpicker::className(), [
+        echo $form->field($model, 'color')->widget(Colorpicker::className(), [
+        ])->textInput(['maxlength' => 7]);
+        ?>
 
-    ]);?>
-    
         <div class="form-group text-center">
             <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'CREATE') : Yii::t('app', 'UPDATE'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
         </div>
