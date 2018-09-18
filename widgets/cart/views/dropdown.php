0182ff603cb4fe58f958802776d1880e8987eabd
Andrew <andrew.panix@gmail.com>
2018-09-18 19:01:47 +0300

    init

--- widgets/cart/views/dropdown.php
+++ widgets/cart/views/dropdown.php
@@ -1,57 +1,60 @@
 <?php
 use yii\helpers\Html;
 use panix\mod\shop\models\Product;
+
 ?>
 <div class="cart">
 
-    
 
-        <?php if ($count > 0) { ?>
+    <?php if ($count > 0) { ?>
         <div class="dropdown">
-        <div class="cart-info dropdown-toggle" id="cart-items" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-            <span class="count"><?= $count ?></span>
-            <span><?= $total; ?></span> <small><?= $currency->symbol; ?></small>
-        </div>
-        <div class="dropdown-menu dropdown-menu-right">
-            
-            <ul class="list-group">
-  <li class="list-group-item">Cras justo odio</li>
-  <li class="list-group-item">Dapibus ac facilisis in</li>
-  <li class="list-group-item">Morbi leo risus</li>
-  <li class="list-group-item">Porta ac consectetur ac</li>
-  <li class="list-group-item">Vestibulum at eros</li>
-</ul>
-            <?php
-            foreach ($items as $product) {
+            <div class="cart-info dropdown-toggle" id="cart-items" data-toggle="dropdown" aria-haspopup="true"
+                 aria-expanded="true">
+                <span class="count"><?= $count ?></span>
+                <span><?= $total; ?></span>
+                <small><?= $currency->symbol; ?></small>
+            </div>
+            <div class="dropdown-menu dropdown-menu-right">
 
-                ?>
-            
+                <ul class="list-group">
+                    <li class="list-group-item">Cras justo odio</li>
+                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                    <li class="list-group-item">Morbi leo risus</li>
+                    <li class="list-group-item">Porta ac consectetur ac</li>
+                    <li class="list-group-item">Vestibulum at eros</li>
+                </ul>
                 <?php
-                $price = Product::calculatePrices($product['model'], $product['variant_models'], $product['configurable_id']);
-                ?>
-                <div class="cart-product-item">
-                    <div class="cart-product-item-image">
-                        <?php echo Html::img($product['model']->getMainImageUrl('50x50'), array('class' => 'img-thumbnail')) ?>
-                    </div>
-                    <div class="cart-product-item-detail">
-                        <?php echo Html::a($product['model']->name, $product['model']->getUrl()) ?>
-                        <br/>
-                        (<?php echo $product['quantity'] ?>)
-                        <?= Yii::$app->currency->number_format(Yii::$app->currency->convert($price)) ?> <?= $currency->symbol; ?>
-                    </div>
-                </div>
+                foreach ($items as $product) {
 
-            <?php } ?>
-            <div class="cart-detail clearfix">
-                <span class="total-price pull-left"><span class="label label-success"><?= $total ?></span> <?= $currency->symbol; ?></span>
-                <?= Html::a(Yii::t('cart/default', 'BUTTON_CHECKOUT'), array('/cart'), array('class' => 'btn btn-sm btn-primary pull-right')) ?>
+                    ?>
+
+                    <?php
+                    $price = Product::calculatePrices($product['model'], $product['variant_models'], $product['configurable_id']);
+                    ?>
+                    <div class="cart-product-item">
+                        <div class="cart-product-item-image">
+                            <?php echo Html::img($product['model']->getMainImageUrl('50x50'), array('class' => 'img-thumbnail')) ?>
+                        </div>
+                        <div class="cart-product-item-detail">
+                            <?php echo Html::a($product['model']->name, $product['model']->getUrl()) ?>
+                            <br/>
+                            (<?php echo $product['quantity'] ?>)
+                            <?= Yii::$app->currency->number_format(Yii::$app->currency->convert($price)) ?> <?= $currency->symbol; ?>
+                        </div>
+                    </div>
+
+                <?php } ?>
+                <div class="cart-detail clearfix">
+                    <span class="total-price pull-left"><span
+                                class="label label-success"><?= $total ?></span> <?= $currency->symbol; ?></span>
+                    <?= Html::a(Yii::t('cart/default', 'BUTTON_CHECKOUT'), array('/cart'), array('class' => 'btn btn-sm btn-primary pull-right')) ?>
+                </div>
             </div>
         </div>
-    </div>
-        <?php } else { ?>
+    <?php } else { ?>
         <a href="/cart" class="cart-info">
             <span class="hidden-xs"><?= Yii::t('cart/default', 'CART_EMPTY') ?></span>
-              </a>
-        <?php } ?>
-  
+        </a>
+    <?php } ?>
+
 </div>
