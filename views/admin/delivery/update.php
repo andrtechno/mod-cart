853a5bba3579d9e7ac740b21092d2ed96af289cf
Andrew <andrew.panix@gmail.com>
2018-09-21 07:38:18 +0300

    init

--- views/admin/delivery/update.php
+++ views/admin/delivery/update.php
@@ -5,52 +5,41 @@ use panix\engine\bootstrap\ActiveForm;
 use yii\helpers\ArrayHelper;
 use panix\mod\cart\models\Payment;
 use panix\ext\tinymce\TinyMce;
+
 ?>
 
 
-
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h3 class="panel-title"><?= Html::encode($this->context->pageName) ?></h3>
+<div class="card bg-light">
+    <div class="card-header">
+        <h5><?= Html::encode($this->context->pageName) ?></h5>
     </div>
-    <div class="panel-body">
-
-
+    <div class="card-body">
         <?php
         $form = ActiveForm::begin([
-                    'options' => ['class' => 'form-horizontal']
+            'options' => ['class' => 'form-horizontal']
         ]);
         ?>
-    <?php
-
-
-
-?>
-<?= $form->field($model, 'name')->textInput(['maxlength' => 255]) ?>
+        <?php
+        ?>
+        <?= $form->field($model, 'name')->textInput(['maxlength' => 255]) ?>
         <?= $form->field($model, 'price')->textInput(['maxlength' => 255]) ?>
         <?= $form->field($model, 'free_from')->textInput(['maxlength' => 255]) ?>
-                                <?=
-                $form->field($model, 'payment_methods')->dropDownList(ArrayHelper::map(Payment::find()->all(), 'id', 'name'), [
-                    'prompt' => '-- payment --'
-                ]);
-                ?>
+        <?=
+        $form->field($model, 'payment_methods')->dropDownList(ArrayHelper::map(Payment::find()->all(), 'id', 'name'), [
+            'prompt' => '-- payment --'
+        ]);
+        ?>
         <div id="payment_configuration"></div>
-<?= $form->field($model, 'description')->widget(TinyMce::class, [
-    'options' => ['rows' => 6]
-]);
-?>
+        <?= $form->field($model, 'description')->widget(TinyMce::class, [
+            'options' => ['rows' => 6]
+        ]);
+        ?>
 
 
         <div class="form-group text-center">
             <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'CREATE') : Yii::t('app', 'UPDATE'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
         </div>
-
-
-
         <?php ActiveForm::end(); ?>
-
-
-
     </div>
 </div>
 
--- views/admin/settings/index.php
+++ views/admin/settings/index.php
@@ -4,40 +4,41 @@ use panix\engine\Html;
 use panix\engine\bootstrap\ActiveForm;
 use panix\ext\taginput\TagInput;
 use panix\ext\tinymce\TinyMce;
+
 $form = ActiveForm::begin([
-            'options' => ['class' => 'form-horizontal'],
-        ]);
+    'options' => ['class' => 'form-horizontal'],
+]);
 ?>
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h3 class="panel-title"><?= $this->context->pageName ?></h3>
-    </div>
-    <div class="panel-body">
-        <?=
-                $form->field($model, 'order_emails')
+    <div class="card bg-light">
+        <div class="card-header">
+            <h5><?= $this->context->pageName ?></h5>
+        </div>
+        <div class="card-body">
+            <?=
+            $form->field($model, 'order_emails')
                 ->widget(TagInput::class, ['placeholder' => 'E-mail'])
                 ->hint('Введите E-mail и нажмите Enter');
-        ?>
+            ?>
 
-        <?= $form->field($model, 'tpl_subject_user'); ?>
-        <?= $form->field($model, 'tpl_subject_admin'); ?>
-
-
-<?= $form->field($model, 'tpl_body_user')->widget(TinyMce::class, [
-    'options' => ['rows' => 6],
-
-]);
-?>
-<?= $form->field($model, 'tpl_body_admin')->widget(TinyMce::class, [
-    'options' => ['rows' => 6],
-
-]);
-?>
+            <?= $form->field($model, 'tpl_subject_user'); ?>
+            <?= $form->field($model, 'tpl_subject_admin'); ?>
 
 
+            <?= $form->field($model, 'tpl_body_user')->widget(TinyMce::class, [
+                'options' => ['rows' => 6],
+
+            ]);
+            ?>
+            <?= $form->field($model, 'tpl_body_admin')->widget(TinyMce::class, [
+                'options' => ['rows' => 6],
+
+            ]);
+            ?>
+
+
+        </div>
+        <div class="card-footer text-center">
+            <?= Html::submitButton(Yii::t('app', 'SAVE'), ['class' => 'btn btn-success']) ?>
+        </div>
     </div>
-    <div class="panel-footer text-center">
-        <?= Html::submitButton(Yii::t('app', 'SAVE'), ['class' => 'btn btn-success']) ?>
-    </div>
-</div>
 <?php ActiveForm::end(); ?>
