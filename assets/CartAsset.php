c912cb08fc093389c94642fec4f8c5099253be8b
Andrew <andrew.panix@gmail.com>
2019-02-08 06:59:43 +0200

    1. change js assets to \yii\web\View::POS_END 2. change all url javascript:void() to hash "#" for bootstrap 4.2.x

--- assets/CartAsset.php
+++ assets/CartAsset.php
@@ -7,10 +7,7 @@ use yii\web\AssetBundle;
 class CartAsset extends AssetBundle {
 
     public $sourcePath = '@cart/assets';
-   // public $sourcePath = '@vendor/panix/mod-cart/assets';
-    public $jsOptions = array(
-        'position' => \yii\web\View::POS_END
-    );
+
     public $js = [
         'cart.js',
         'number_format.js',
