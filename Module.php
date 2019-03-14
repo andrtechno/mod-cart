72b70e829e66f2efc56291e9c9a19103e9aeba8f
Andrew <andrew.panix@gmail.com>
2019-03-14 13:53:39 +0200

    111

--- Module.php
+++ Module.php
@@ -7,16 +7,19 @@ use panix\engine\WebModule;
 use panix\mod\cart\models\Order;
 use yii\base\BootstrapInterface;
 
-class Module extends WebModule implements BootstrapInterface {
+class Module extends WebModule implements BootstrapInterface
+{
 
     public $icon = 'cart';
 
-    public function init() {
-        $count = Order::find()->where(['status_id' => 1])->count();
-        $this->count['num'] = $count;
-        $this->count['label'] = Yii::t('cart/default', 'WP_COUNT', ['num' => $this->count['num']]);
-        $this->count['url'] = ['/admin/cart', 'OrderSearch[status_id]' => 1];
-
+    public function init()
+    {
+        if (!(Yii::$app instanceof yii\console\Application)) {
+            $count = Order::find()->where(['status_id' => 1])->count();
+            $this->count['num'] = $count;
+            $this->count['label'] = Yii::t('cart/default', 'WP_COUNT', ['num' => $this->count['num']]);
+            $this->count['url'] = ['/admin/cart', 'OrderSearch[status_id]' => 1];
+        }
         parent::init();
     }
 
@@ -40,7 +43,9 @@ class Module extends WebModule implements BootstrapInterface {
             'cart' => ['class' => 'panix\mod\cart\components\Cart'],
         ]);
     }
-    public function getInfo() {
+
+    public function getInfo()
+    {
         return [
             'label' => Yii::t('cart/default', 'MODULE_NAME'),
             'author' => 'andrew.panix@gmail.com',
@@ -51,7 +56,8 @@ class Module extends WebModule implements BootstrapInterface {
         ];
     }
 
-    public function getAdminMenu2() {
+    public function getAdminMenu2()
+    {
         return [
             'cart' => [
                 'label' => Yii::t('cart/default', 'MODULE_NAME'),
@@ -91,7 +97,9 @@ class Module extends WebModule implements BootstrapInterface {
             ],
         ];
     }
-    public function getAdminSidebar() {
+
+    public function getAdminSidebar()
+    {
         $mod = new \panix\engine\bootstrap\Nav;
         $items = $mod->findMenu($this->id);
         return $items['items'];
