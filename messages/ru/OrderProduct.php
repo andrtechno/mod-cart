967bfac01ba90d213ca96763c3bf04185935ee71
Andrew <andrew.panix@gmail.com>
2019-03-20 20:41:15 +0200

    111

---
+++ messages/ru/OrderProduct.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'ORDER_ID' => '№ заказа',
+    'PRODUCT_ID' => 'Продукт',
+    'SUPPLIER_ID' => 'Поставщик',
+    'CONFIGURABLE_ID' => 'Конфигурация',
+    'NAME' => 'Название',
+    'QUANTITY' => 'Количество',
+    'SKU' => 'Артикул',
+    'PRICE' => 'Цена',
+    'CATEGORIES' => 'Категории',
+];
