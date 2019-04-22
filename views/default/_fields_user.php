9ff99b1147efbffa939ca56c725ecc28c79af78d
Andrew <andrew.panix@gmail.com>
2019-04-22 04:30:02 +0300

    init

--- views/default/_fields_user.php
+++ views/default/_fields_user.php
@@ -2,6 +2,7 @@
 
 use panix\engine\Html;
 use panix\engine\widgets\inputmask\InputMask;
+
 /**
  * @var $form \yii\widgets\ActiveForm
  * @var $model \panix\mod\cart\models\forms\OrderCreateForm
@@ -18,9 +19,22 @@ use panix\engine\widgets\inputmask\InputMask;
 
 
 <div class="form-group">
-    <?= $form->field($model, 'user_email'); ?>
+    <?php // $form->field($model, 'user_email'); ?>
 </div>
+<?php
+/*echo $form->field($model, 'user_email')->widget(\panix\ext\inputmask\InputMask::class, [
+    'pluginOptions' => [
+        'mask' => '999-99-99'
+    ]
+]);*/
 
+echo $form->field($model, 'user_email')->widget(\panix\ext\inputmask\InputMask::class, [
+    'extensions' => ['date'],
+    'pluginOptions' => [
+        'mask' => 'dd/mm/yyyy'
+    ]
+]);
+?>
 
 <div class="form-group">
     <?= $form->field($model, 'user_comment')->textarea(); ?>
