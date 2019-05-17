019a12c0516459a5a18af3fbddfe52aa58b88e0f
Andrew <andrew.panix@gmail.com>
2019-05-17 12:40:15 +0300

    init

--- CartAsset.php
+++ CartAsset.php
@@ -2,11 +2,12 @@
 
 namespace panix\mod\cart;
 
-use yii\web\AssetBundle;
+use panix\engine\web\AssetBundle;
 
-class CartAsset extends AssetBundle {
+class CartAsset extends AssetBundle
+{
 
-    public $sourcePath = __DIR__.'/assets';
+    public $sourcePath = __DIR__ . '/assets';
 
     public $js = [
         'cart.js',
