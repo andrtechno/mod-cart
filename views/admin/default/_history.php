f6f3e889607a73cd93a9f0b40f6b43d6ad150179
Andrew <andrew.panix@gmail.com>
2019-11-21 19:00:33 +0200

    init

--- views/admin/default/_history.php
+++ views/admin/default/_history.php
@@ -1,29 +1,35 @@
 <?php
+/**
+ * @var \panix\mod\cart\models\Order $model
+ */
 $history = $model->getHistory();
 
 if (empty($history)) {
 
-    //echo Yii::app->tpl->alert('info', Yii::t('CartModule.admin', 'HISTORY_EMPTY'), false);
-    echo Yii::t('cart/admin', 'HISTORY_EMPTY');
+    echo \panix\engine\bootstrap\Alert::widget([
+        'options' => ['class' => 'alert-info'],
+        'body' => Yii::t('cart/admin', 'HISTORY_EMPTY')
+    ]);
+
     return;
 }
 ?>
 
 <table class="table table-striped table-bordered">
     <thead>
-        <tr>
-            <th></th>
-            <th><?= Yii::t('cart/admin', 'BEFORE'); ?></th>
-            <th><?= Yii::t('cart/admin', 'AFTER'); ?></th>
-        </tr>
+    <tr>
+        <th></th>
+        <th><?= Yii::t('cart/admin', 'BEFORE'); ?></th>
+        <th><?= Yii::t('cart/admin', 'AFTER'); ?></th>
+    </tr>
     </thead>
     <tbody>
-        <?php
-        foreach ($history as $row) {
-            echo $this->render('_' . $row->handler, [
-                'data' => $row,
-            ]);
-        }
-        ?>
+    <?php
+    foreach ($history as $row) {
+        echo $this->render('_' . $row->handler, [
+            'data' => $row,
+        ]);
+    }
+    ?>
     </tbody>
 </table>
