cb8a40a61457ea19f24b0bc244883a4f5d09e858
Andrew <andrew.panix@gmail.com>
2023-06-18 08:51:08 +0300

    2222

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -234,7 +234,6 @@ class DefaultController extends WebController
             $order = $this->createOrder();
 
             if ($order instanceof Order) {
-                die('ok');
                 Yii::$app->cart->clear();
                 Yii::$app->session->setFlash('success', Yii::t('cart/default', 'SUCCESS_ORDER'));
                 return $this->redirect(['view', 'secret_key' => $order->secret_key]);
