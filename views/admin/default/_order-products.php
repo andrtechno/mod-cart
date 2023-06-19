03dc018eff133fb3019961c09d86034956f06277
Andrew <andrew.panix@gmail.com>
2023-06-19 19:05:07 +0300

    123123

--- views/admin/default/_order-products.php
+++ views/admin/default/_order-products.php
@@ -44,7 +44,6 @@ echo GridView::widget([
             'class' => 'panix\engine\grid\columns\ImageColumn',
             'attribute' => 'image',
             'header' => Yii::t('cart/OrderProduct', 'IMAGE'),
-            // 'filter'=>true,
             'value' => function ($model) {
                 /** @var $model OrderProduct */
                 if ($model->getProduct()) {
@@ -95,16 +94,7 @@ echo GridView::widget([
             'footer' => Yii::$app->currency->number_format($model->total_price) . ' ' . Yii::$app->currency->main['symbol'],
             'value' => function ($model) {
                 /** @var $model OrderProduct */
-                //if ($model->currency_id && $model->currency_rate) {
-                //    $priceValue = Yii::$app->currency->convert($model->price, $model->currency_id);
-                // } else {
-
-                //  }
-                //if ($model->discount) {
-                //    $priceValue = ($model->price) * $model->quantity; // - $model->discount
-                //} else {
                 $priceValue = $model->price * $model->in_box * $model->quantity;
-                //}
                 return Yii::$app->currency->number_format($priceValue) . ' ' . Yii::$app->currency->main['symbol'];
             }
         ],
@@ -161,21 +151,22 @@ Pjax::end();
                 <li class="list-group-item">
                     Бонусы к зачаслению:
                     <h5 class="m-0 float-right"><?= floor($model->total_price * Yii::$app->settings->get('user', 'bonus_ratio')) ?>
-                        <span class="text-muted"><?= $symbol ?></span></h5>
+                        <span class="text-muted"><?= $symbol ?></span>
+                    </h5>
                 </li>
             <?php } ?>
             <?php if ($model->delivery_price > 0) { ?>
                 <li class="list-group-item">
-                    <?= Yii::t('cart/Order', 'DELIVERY_PRICE') ?>: <strong
-                            class="float-right"><?= Yii::$app->currency->number_format($model->delivery_price); ?> <?= $symbol; ?></strong>
+                    <?= Yii::t('cart/Order', 'DELIVERY_PRICE') ?>:
+                    <strong class="float-right"><?= Yii::$app->currency->number_format($model->delivery_price); ?> <?= $symbol; ?></strong>
                 </li>
             <?php } ?>
             <li class="list-group-item">
-                <?= Yii::t('cart/default', 'ORDER_PRICE') ?>: <strong
-                        class="float-right"><span
-                            class="total-price"><?= Yii::$app->currency->number_format($model->total_price) ?></span>
-                    <span
-                            class="text-muted"><?= $symbol ?></span></strong>
+                <?= Yii::t('cart/default', 'ORDER_PRICE') ?>:
+                <strong class="float-right">
+                    <span class="total-price"><?= Yii::$app->currency->number_format($model->total_price) ?></span>
+                    <span class="text-muted"><?= $symbol ?></span>
+                </strong>
             </li>
             <?php if ($model->discount) { ?>
                 <li class="list-group-item">
@@ -183,8 +174,10 @@ Pjax::end();
                     <?php if ('%' === substr($model->discount, -1, 1)) { ?>
                         <strong class="float-right"><?= $model->discount; ?></strong>
                     <?php } else { ?>
-                        <strong class="float-right"><?= Yii::$app->currency->number_format($model->discount) ?> <span
-                                    class="text-muted"><?= $symbol ?></span></strong>
+                        <strong class="float-right">
+                            <?= Yii::$app->currency->number_format($model->discount) ?>
+                            <span class="text-muted"><?= $symbol ?></span>
+                        </strong>
                     <?php } ?>
                 </li>
             <?php } ?>
