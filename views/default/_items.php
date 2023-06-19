15b5a4ff651177f6a72ef2ced45a07b2838483d1
Andrew <andrew.panix@gmail.com>
2023-06-19 12:21:23 +0300

    items

--- views/default/_items.php
+++ views/default/_items.php
@@ -12,10 +12,10 @@ use yii\helpers\Url;
             <th scope="col" style="width:500px"><?= Yii::t('cart/default', 'TABLE_PRODUCT') ?></th>
 
             <th scope="col"><?= Yii::t('cart/default', 'QUANTITY') ?></th>
-            <th scope="col"><?= Yii::t('default','QUANTITY_PAIRS'); ?></th>
-            <th scope="col"><?= Yii::t('default','PRICE_PER'); ?></th>
-            <th scope="col"><?= Yii::t('default','PRICE_PER_BOX'); ?></th>
-            <th scope="col"><?= Yii::t('default','SUM'); ?></th>
+            <th scope="col"><?= Yii::t('default', 'QUANTITY_PAIRS'); ?></th>
+            <th scope="col"><?= Yii::t('default', 'PRICE_PER'); ?></th>
+            <th scope="col"><?= Yii::t('default', 'PRICE_PER_BOX'); ?></th>
+            <th scope="col"><?= Yii::t('default', 'SUM'); ?></th>
             <th class="cart-delete-row"></th>
         </tr>
         </thead>
@@ -27,7 +27,7 @@ use yii\helpers\Url;
             <tr id="product-<?= $index; ?>">
 
                 <td class="none">
-                    <?= Html::a('<i class="icon-delete"></i> <span class="d-inline-block d-lg-none">'.Yii::t('app/default','DELETE').'</span>', ['/cart/default/remove', 'id' => $index], ['data-product' => $index, 'class' => 'cart-remove remove d-block d-md-none mb-3']) ?>
+                    <?= Html::a('<i class="icon-delete"></i> <span class="d-inline-block d-lg-none">' . Yii::t('app/default', 'DELETE') . '</span>', ['/cart/default/remove', 'id' => $index], ['data-product' => $index, 'class' => 'cart-remove remove d-block d-md-none mb-3']) ?>
                     <div class="d-flex align-items-center">
                         <div class="text-left">
                             <?= Html::a(Html::img(Url::to($product['model']->getMainImage('240x')->url), ['class' => 'img-fluid1', 'width' => 120, 'alt' => $product['model']->name]), $product['model']->getUrl()); ?>
@@ -44,8 +44,8 @@ use yii\helpers\Url;
                         <div class="spinner ">
                             <?= Html::button('-', ['class' => 'spinner--down', 'data-event' => 'down']); ?>
                             <?= Html::textInput("quantities[$index]", $product['quantity'], ['data' => [
-                                'step' => $product['model']->in_box,
-                                'min' => $product['model']->quantity_min,
+                                'step' => (Yii::$app->settings->get('cart', 'quantity_convert')) ? $product['model']->in_box : 1,
+                                'min' => (Yii::$app->settings->get('cart', 'quantity_convert')) ? $product['model']->quantity_min : 1,
                                 //'max' => 999,
                                 'product_id' => $index
                             ]]); ?>
@@ -53,32 +53,29 @@ use yii\helpers\Url;
                         </div>
                     </div>
                 </td>
-                <td data-label="<?= Yii::t('default','PER_BOX'); ?>"><span
-                            class="price"><?= $product['in_box']; ?></span></td>
-                <td data-label="<?= Yii::t('default','PRICE_PER'); ?>">
+                <td data-label="<?= Yii::t('default', 'PER_BOX'); ?>">
+                    <span class="price"><?= $product['in_box']; ?></span>
+                </td>
+                <td data-label="<?= Yii::t('default', 'PRICE_PER'); ?>">
                        <span class="price">
-                              <?= Yii::$app->currency->number_format($price / $product['in_box']); ?>
+                              <?= Yii::$app->currency->number_format($price); ?>
                               <span class="currency"><?= Yii::$app->currency->active['symbol']; ?></span>
                        </span>
                 </td>
-                <td data-label="<?= Yii::t('default','PRICE_PER_BOX'); ?>">
+                <td data-label="<?= Yii::t('default', 'PRICE_PER_BOX'); ?>">
                        <span class="price">
-
-                                                <?= Yii::$app->currency->number_format($price); ?>
-                                                <span class="currency"><?= Yii::$app->currency->active['symbol']; ?></span>
-
+                            <?= Yii::$app->currency->number_format($price * $product['in_box']); ?>
+                            <span class="currency"><?= Yii::$app->currency->active['symbol']; ?></span>
                        </span>
                 </td>
-                <td data-label="<?= Yii::t('default','SUM'); ?>" id="price-<?= $index ?>">
-
+                <td data-label="<?= Yii::t('default', 'SUM'); ?>" id="price-<?= $index ?>">
                                 <span class="price">
-                                    <span class="row-total-price<?= $index ?>"><?= Yii::$app->currency->number_format($price / $product['in_box'] * $product['quantity']); ?></span>
+                                    <span class="row-total-price<?= $index ?>"><?= Yii::$app->currency->number_format($price * $product['in_box'] * $product['quantity']); ?></span>
                                      <span class="currency"><?= Yii::$app->currency->active['symbol']; ?></span>
                                 </span>
-
                 </td>
                 <td class="cart-delete-row text-left text-lg-center">
-                    <?= Html::a('<i class="icon-delete"></i> <span class="d-inline-block d-lg-none">'.Yii::t('app/default','DELETE').'</span>', ['/cart/default/remove', 'id' => $index,'popup'=>$popup], ['data-product' => $index, 'class' => 'cart-remove remove d-none d-md-block']) ?>
+                    <?= Html::a('<i class="icon-delete"></i> <span class="d-inline-block d-lg-none">' . Yii::t('app/default', 'DELETE') . '</span>', ['/cart/default/remove', 'id' => $index, 'popup' => $popup], ['data-product' => $index, 'class' => 'cart-remove remove d-none d-md-block']) ?>
                 </td>
             </tr>
         <?php } ?>
